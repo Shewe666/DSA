29fa7bfe36072db1f1a6367e30418d9ca44e14e8
Shivi Mishra <shivim457@gmail.com>
2026-07-21 01:39:07 +0530

    Time: 2 ms (87.05%), Space: 49.3 MB (54.2%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.java
+++ 0143-reorder-list/0143-reorder-list.java
@@ -10,31 +10,41 @@
  */
 class Solution {
     public void reorderList(ListNode head) {
-    if(head.next==null||head.next.next==null){
-        return;
-    }
-    ArrayList<ListNode> list=new ArrayList<>();
-    ListNode curr=head;
+   //optimized approach:
+   if(head==null && head.next==null){
+    return;
+   }
+   ListNode slow=head;
+   ListNode fast=head;
+   //first middle condition inside thw hile loop
+   while(fast.next!=null && fast.next.next!=null ){
+    slow=slow.next;
+    fast=fast.next.next;
+   }
+   //reverse second half  logic 
+   ListNode second=slow.next;
+   slow.next=null;
+   ListNode prev=null;
+   ListNode curr=second;
 
-    while(curr!=null){
-        list.add(curr);
-        curr=curr.next;
-    }
-    int left=0;
-    int right=list.size()-1;
-    while(left<right){
-        list.get(left).next=list.get(right);
-        left++;
-        //for even elememts this condition needs to be between the left and the right 
+   while(curr!=null){
+   ListNode next=curr.next;
+   curr.next=prev;
+   prev=curr;
+   curr=next;
+   }
 
-        if(left==right){
-            break;
-        }
-        list.get(right).next=list.get(left);
-        right--;
+   ListNode first=head;
+   second=prev;
+   //in half linked list reversal we will do second!=null cpz second half will be null first
+   while(second!=null){
+    ListNode next1=first.next;
+    ListNode next2=second.next;
+    first.next=second;
+    second.next=next1;
 
-    }
-    //last node that is left must be connected with null;
-    list.get(left).next=null;
+    first=next1;
+    second=next2;
+   }
     }
 }
