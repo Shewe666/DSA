20042d3723aec9c2d07fe515f1d7740fd749f267
Shivi Mishra <shivim457@gmail.com>
2026-07-21 10:28:05 +0530

    Time: 0 ms (100%), Space: 43.8 MB (51.42%) - LeetHub

--- 0086-partition-list/0086-partition-list.java
+++ 0086-partition-list/0086-partition-list.java
@@ -11,29 +11,26 @@
 class Solution {
     public ListNode partition(ListNode head, int x) {
        
-        ListNode smallDummy = new ListNode(0);
-        ListNode largeDummy = new ListNode(0);
+      ListNode smallDummy=new ListNode(0);
+      ListNode largeDummy=new ListNode(0);
 
-        ListNode small = smallDummy;
-        ListNode large = largeDummy;
+      ListNode small=smallDummy;
+      ListNode large=largeDummy;
 
-        ListNode curr = head;
-
-        while (curr != null) {
-            if (curr.val < x) {
-                small.next = curr;
-                small = small.next;
-            } else {
-                large.next = curr;
-                large = large.next;
-            }
-
-            curr = curr.next;
+      ListNode curr=head;
+      while(curr!=null){
+        if(curr.val<x){
+            small.next=curr;
+            small=small.next;
         }
-
-        small.next = largeDummy.next;
-        large.next = null;
-
-        return smallDummy.next;
+        else{
+            large.next=curr;
+            large=large.next;
+        }
+        curr=curr.next;
+      }
+      small.next=largeDummy.next;
+      large.next=null;
+      return smallDummy.next;
     }
 }
