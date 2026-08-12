8a0c76fa2a876e028c25ef97bd3aa684da5718c0
Shivi Mishra <shivim457@gmail.com>
2026-08-13 00:43:07 +0530

    Time: 0 ms (100%), Space: 45.5 MB (77.56%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -15,49 +15,27 @@
  */
 class Solution {
     public boolean isBalanced(TreeNode root) {
-        //bfs o(n2) time complexity
-        if( root == null){
-            return true;
-        }
-        Queue<TreeNode> q = new LinkedList<>();
-        q.offer(root);
-        while(!q.isEmpty()){
-            TreeNode node = q.poll();
-            int leftHeight = height(node.left);
-            int rightHeight = height(node.right);
-             
-            if (Math.abs(leftHeight-rightHeight)>1){
-                return false;
-            }
-            if(node.left!= null){
-                q.offer(node.left);
-            }
-            if(node.right!= null){
-                q.offer(node.right);
-            }
+        if(height(root) == -1){
+            return false;
         }
         return true;
     }
     private int height(TreeNode root){
-        if(root == null){
+        if(root==null){
             return 0;
         }
-        Queue<TreeNode> q = new LinkedList<>();
-        q.offer(root);
-        int height=0;
-        while(!q.isEmpty()){
-        int size = q.size();
-        for( int i =0;i< size;i++){
-            TreeNode node = q.poll();
-            if(node.left!= null){
-                q.offer(node.left);
-            }
-            if(node.right!= null){
-                q.offer(node.right);
-         }
+        int left = height(root.left);
+        if( left == -1){
+            return -1;
         }
-        height ++;  
-     }
-     return height;
+        int right =  height(root.right);
+        if( right == -1){
+            return -1;
+        }
+
+        if( Math.abs(left-right)>1){
+            return -1;
+        }
+        return Math.max(left ,right)+1; 
     }
 }
