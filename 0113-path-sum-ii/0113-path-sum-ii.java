beb821d92c7b44628c37465834ff70d40a1234a2
Shivi Mishra <shivim457@gmail.com>
2026-08-21 14:00:30 +0530

    Time: 1 ms (99.98%), Space: 45.5 MB (39.83%) - LeetHub

--- 0113-path-sum-ii/0113-path-sum-ii.java
+++ 0113-path-sum-ii/0113-path-sum-ii.java
@@ -16,21 +16,25 @@
 class Solution {
     List<List<Integer>> ans = new ArrayList<>();
     public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
-        dfs(root, targetSum, new ArrayList<>());
-        return ans;
+    dfs(root, targetSum, new ArrayList<>());
+    return ans;
     }
     public void dfs(TreeNode root, int targetSum, List<Integer> path){
-        if( root == null){
-            return;
+        //base case
+     if( root ==null){
+        return ;
+     }
+     path.add(root.val);
+     if( root.left == null && root.right == null){
+        if(targetSum == root.val){
+            ans.add(new ArrayList<>(path));
         }
-        path.add(root.val);
-        if( root.left == null && root.right == null && targetSum == root.val){
-             ans.add(new ArrayList<>(path));
-        }
-        dfs(root.left, targetSum - root.val , path);
-        dfs(root.right, targetSum - root.val , path);
+     }
+     dfs(root.left, targetSum - root.val , path);
+     dfs( root.right , targetSum - root.val, path);
 
-        path.remove(path.size()-1);
-
+    // after one function call is  over and if that is not the valid path we will again get back to the parent function after executing this line 
+    path.remove(path.size()-1);
     }
+ 
 }
