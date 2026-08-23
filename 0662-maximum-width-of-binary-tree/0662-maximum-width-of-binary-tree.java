ca722f77aa84b6fa9165b6963e6e2fd4324c7545
Shivi Mishra <shivim457@gmail.com>
2026-08-24 00:55:17 +0530

    Time: 1 ms (99.69%), Space: 45 MB (79.56%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.java
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.java
@@ -15,58 +15,37 @@
  */
 class Solution {
 
-    class Pair {
+    class Pair{
         TreeNode node;
-        long index;
-
-        Pair(TreeNode node, long index) {
-            this.node = node;
-            this.index = index;
-        }
+        int index;
+    
+    public Pair(TreeNode node, int index){
+        this.node = node;
+        this.index = index;
     }
-
+    }
     public int widthOfBinaryTree(TreeNode root) {
-
-        if (root == null)
-            return 0;
-
-        Queue<Pair> queue = new LinkedList<>();
-
-        queue.offer(new Pair(root, 0));
-
-        int maxWidth = 0;
-
-        while (!queue.isEmpty()) {
-
-            int size = queue.size();
-
-            long first = queue.peek().index;
-            long last = first;
-
-            for (int i = 0; i < size; i++) {
-
-                Pair curr = queue.poll();
-
-                last = curr.index;
-
-                if (curr.node.left != null) {
-                    queue.offer(
-                        new Pair(curr.node.left, 2 * curr.index + 1)
-                    );
+        Queue<Pair> q = new LinkedList<>();
+        q.offer(new Pair(root,0));// root and initial index 0
+        int maxWidth=0;
+        while(!q.isEmpty()){
+            int size = q.size();
+            int first = q.peek().index;
+            int last = first;
+            for( int i = 0 ; i<size ;i++){
+                Pair curr = q.poll();
+                last=curr.index;
+                if(curr.node.left!=null){
+                    q.offer(new Pair(curr.node.left, 2*curr.index+1));
                 }
-
-                if (curr.node.right != null) {
-                    queue.offer(
-                        new Pair(curr.node.right, 2 * curr.index + 2)
-                    );
-                }
+                if(curr.node.right!=null){
+                    q.offer(new Pair(curr.node.right, 2*curr.index+2));
+                }   
             }
-
-            int width = (int)(last - first + 1);
-
+            int width = (last - first) +1;
             maxWidth = Math.max(maxWidth, width);
         }
-
         return maxWidth;
+      
     }
 }
