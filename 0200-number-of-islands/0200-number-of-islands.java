00d55b8187f956c8dd30c771a84436525f5082bb
Shivi Mishra <shivim457@gmail.com>
2026-09-03 17:59:43 +0530

    Time: 5 ms (29.29%), Space: 54.6 MB (6.52%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,34 +1,32 @@
 class Solution {
-    int m ;
     int n ;
+    int m;
     public int numIslands(char[][] grid) {
         n = grid.length;
         m= grid[0].length;
-        int islands=0;
         boolean visited[][] = new boolean[n][m];
-
-        for( int i =0;i <n; i++){
-            for(int j =0;j<m;j++){
-                if(grid[i][j]=='1' && !visited[i][j]){
-                    dfs(i,j,grid,visited);
-                    islands++;
+        int island=0;
+        for(int i =0; i<n;i++){
+            for(int j =0;j<m ;j++){
+                if( grid[i][j]=='1' && !visited[i][j]){
+                    dfs(i,j,visited,grid);
+                    island++;
                 }
             }
         }
-        return islands;
+        return island;
 
     }
-    public void dfs(int i,int j,char grid[][] , boolean visited[][]){
-        //out of bound condition and other base condition that we need to skip :
-        if(i<0 || i>=n || j<0 ||j>=m||grid[i][j] =='0'||visited[i][j]){
+    public void dfs(int i , int j , boolean[][] visited, char[][]grid){
+        //base case -<out of bound and other conditions
+        if(i<0 || i>=n||j<0||j>=m ||grid[i][j]=='0' || visited[i][j]){
             return;
         }
         visited[i][j]=true;
-        int adjlist[][]={{i-1,j},{i,j+1},{i+1,j},{i,j-1}};
+        //for neighboours //up //right //down ..left
+        int [][]adjlist= {{i-1,j},{i,j+1},{i+1,j},{i,j-1}};
         for(int neighbour[]: adjlist){
-            dfs(neighbour[0],neighbour[1],grid,visited);
+            dfs(neighbour[0],neighbour[1],visited,grid);
         }
-
-
-    }
+     }
 }
