2fe24a515b1f8b48ce2152260f0d07ec80026efc
Shivi Mishra <shivim457@gmail.com>
2026-09-03 22:42:41 +0530

    Time: 7 ms (14.45%), Space: 51.8 MB (78.31%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -2,31 +2,38 @@ class Solution {
     int n ;
     int m;
     public int numIslands(char[][] grid) {
-        n = grid.length;
-        m= grid[0].length;
-        boolean visited[][] = new boolean[n][m];
-        int island=0;
-        for(int i =0; i<n;i++){
-            for(int j =0;j<m ;j++){
-                if( grid[i][j]=='1' && !visited[i][j]){
-                    dfs(i,j,visited,grid);
-                    island++;
-                }
+        n=grid.length;
+        m = grid[0].length;
+       boolean [][] visited = new boolean [n][m];
+       int island=0;
+       for(int i =0; i<n ;i++){
+        for(int j =0;j<m;j++){
+            if(grid[i][j]=='1' && !visited[i][j]){
+                bfs(i,j,visited,grid);
+                island++;
             }
         }
-        return island;
-
-    }
-    public void dfs(int i , int j , boolean[][] visited, char[][]grid){
-        //base case -<out of bound and other conditions
-        if(i<0 || i>=n||j<0||j>=m ||grid[i][j]=='0' || visited[i][j]){
-            return;
-        }
-        visited[i][j]=true;
-        //for neighboours //up //right //down ..left
-        int [][]adjlist= {{i-1,j},{i,j+1},{i+1,j},{i,j-1}};
-        for(int neighbour[]: adjlist){
-            dfs(neighbour[0],neighbour[1],visited,grid);
-        }
+       }
+       return island;
      }
+    public void bfs(int i , int j , boolean[][] visited , char[][]grid){
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[]{i,j});
+        int size = q.size();
+        while(!q.isEmpty()){
+            int[]curr = q.poll();
+            int row = curr[0];
+            int col = curr[1];
+            int[][] adjlist={{row-1,col},{row,col+1},{row+1,col},{row,col-1}};
+            for(int[] neighbour :adjlist){
+                int nrow=neighbour[0];
+                int ncol=neighbour[1];
+                if(nrow<0 || nrow>=n || ncol<0||ncol>=m||visited[nrow][ncol]||grid[nrow][ncol]=='0'){
+                    continue;
+                }
+                visited[nrow][ncol]=true;
+                q.offer(new int[]{nrow,ncol});
+            }
+        }
+    }
 }
