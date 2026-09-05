6d68be05383e0a954ec7d83c59d007bee2691db9
Shivi Mishra <shivim457@gmail.com>
2026-09-06 04:31:30 +0530

    Time: 0 ms (100%), Space: 43.7 MB (33.52%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.java
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -15,9 +15,7 @@
  */
 class Solution {
     public boolean isSymmetric(TreeNode root) {
-        if( root == null){
-            return true;
-        }
+       
        return isMirror(root, root);
     }
     public boolean isMirror(TreeNode root1, TreeNode root2){
